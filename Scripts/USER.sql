2c8e7899ffaf48c80da5e8b52895b0d85ae2c590
LeonardoFlores1 <leonardoflores150@gmail.com>
2018-04-06 18:23:53 -0600

    usuario leonardo
    crear usuario leonardo clave leonardo

--- Scripts/USER.sql
+++ Scripts/USER.sql
@@ -16,3 +16,23 @@ GRANT create sequence TO DB_FLIPBOARD;
 GRANT create materialized view TO DB_FLIPBOARD;
 GRANT select any table TO DB_FLIPBOARD;
 GRANT create synonym TO DB_FLIPBOARD;
+
+
+--Crear nuevo usuario(esquema) con el password "leonardo" 
+CREATE USER leonardo
+  IDENTIFIED BY "leonardo"
+  DEFAULT TABLESPACE USERS
+  TEMPORARY TABLESPACE TEMP;
+--asignar cuota ilimitada al tablespace por defecto  
+ALTER USER leonardo QUOTA UNLIMITED ON USERS;
+
+--Asignar privilegios basicos
+GRANT create session TO leonardo;
+GRANT create table TO leonardo;
+GRANT create view TO leonardo;
+GRANT create any trigger TO leonardo;
+GRANT create any procedure TO leonardo;
+GRANT create sequence TO leonardo;
+GRANT create materialized view TO leonardo;
+GRANT select any table TO leonardo;
+GRANT create synonym TO leonardo;
