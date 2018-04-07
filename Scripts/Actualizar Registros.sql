6ad84c8e71fe557bc806cc58ef0a34ce46a6ac98
LeonardoFlores1 <leonardoflores150@gmail.com>
2018-04-06 22:20:19 -0600

    actualizar registros

--- Scripts/Actualizar Registros.sql
+++ Scripts/Actualizar Registros.sql
@@ -8,3 +8,8 @@ END;
 @"C:\Users\diazn\Documents\GitHub\BD2-I2018\Scripts\DDL.sql";
 --Insercion de registros nuevamente (Nelson)
 @"C:\Users\diazn\Documents\GitHub\BD2-I2018\Scripts\DML.sql";
+
+--CREACION DE TABLAS (LEONARDO)
+@"C:\Users\Leonardo\Documents\GitHub\BD2-I2018\Scripts\DDL.sql";
+--INSERCION DE DATOS (LEONARDO)
+@"C:\Users\Leonardo\Documents\GitHub\BD2-I2018\Scripts\DML.sql";
